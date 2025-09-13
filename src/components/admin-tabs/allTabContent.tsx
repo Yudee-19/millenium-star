f0fb5202bd75512a40fa3d689a3fb59347ae6ac0
Uday Singh <19.yudee@gmail.com>
2025-09-13 23:27:18 +0530

    added rapaport tab in admin

--- src/components/admin-tabs/allTabContent.tsx
+++ src/components/admin-tabs/allTabContent.tsx
@@ -6,6 +6,7 @@ import { DiamondTableToolbar } from "../data-table/diamond-toolbar";
 import { DiamondCharts } from "../charts/diamond-charts";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { GridIcon, CircleCheckBig, ChartColumn, Ruler } from "lucide-react";
+import Rapaport from "../rapaport/rapaport";
 
 interface AllTabContentProps {
     diamonds: any[];
@@ -82,8 +83,8 @@ const AllTabContent: React.FC<AllTabContentProps> = ({
                 />
             </div>
 
-            <Tabs defaultValue="tableview" className="">
-                <TabsList className="rounded-md space-x-3">
+            <Tabs defaultValue="tableview" className=" ">
+                <TabsList className="rounded-md space-x-3 border-1">
                     <TabsTrigger
                         value="tableview"
                         className="rounded-md text-black p-3"
@@ -97,28 +98,37 @@ const AllTabContent: React.FC<AllTabContentProps> = ({
                     >
                         Chart
                     </TabsTrigger>
+                    <TabsTrigger
+                        value="rapaport"
+                        className="rounded-md text-black p-3"
+                    >
+                        Rapaport
+                    </TabsTrigger>
                 </TabsList>
 
-                <TabsContent value="tableview">
-                    <Shell>
-                        <div className="flex h-full min-h-screen overflow-x-auto flex-col">
-                            <div className="flex flex-col space-y-8">
-                                <DataTable
-                                    data={diamonds}
-                                    columns={diamondColumns}
-                                    toolbar={DiamondTableToolbar}
-                                    pageCount={pageCount}
-                                    loading={loading}
-                                    onStateChange={handleTableStateChange}
-                                    paginationMeta={paginationMeta}
-                                />
-                            </div>
+                <TabsContent value="tableview" className=" w-full  ">
+                    {/* <Shell className="h-fit w-full bg-blue-900"> */}
+                    <div className="flex h-full min-w-full  overflow-x-auto w-full flex-col  ">
+                        <div className="flex flex-col space-y-8 w-full  ">
+                            <DataTable
+                                data={diamonds}
+                                columns={diamondColumns}
+                                toolbar={DiamondTableToolbar}
+                                pageCount={pageCount}
+                                loading={loading}
+                                onStateChange={handleTableStateChange}
+                                paginationMeta={paginationMeta}
+                            />
                         </div>
-                    </Shell>
+                    </div>
+                    {/* </Shell> */}
                 </TabsContent>
 
                 <TabsContent value="chart">
                     <DiamondCharts />
+                </TabsContent>
+                <TabsContent value="rapaport">
+                    <Rapaport />
                 </TabsContent>
             </Tabs>
         </>
